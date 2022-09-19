b5c1949f02cc8736f67153b490b66646f532f623
Arul John <arul@cpan.org>
2022-09-19 19:09:57 -0400

    Like, retweet and follow user of tweet

--- telgodubot.py
+++ telgodubot.py
@@ -21,18 +21,18 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 # StreamingClient class
 class TelgoduListener(tweepy.StreamingClient):
     def on_tweet(self, tweet):
-        print(tweet.id, tweet.text)
-        try:
-            client.retweet(tweet.id)
-            time.sleep(5)
-        except Exception as error:
-            print('ERROR:', error)
+        client.retweet(tweet.id)
+        client.like(tweet.id)
+        client.follow_user(tweet.author_id)
+        print(tweet.id, tweet.author_id)
+        time.sleep(30)
 
     def on_exception(self, data):
         print('on_exception', data)
+        exit()
 
 # Stream and retweet
 stream = TelgoduListener(bearer_token=BEARER_TOKEN)
 rule = tweepy.StreamRule("(#python OR #programming OR #cybersecurity OR #web3)")
-stream.add_rules(rule, dry_run=True)
-stream.filter()
+stream.add_rules(rule)
+stream.filter(tweet_fields=["author_id"])
